d90f578013d436f50d6132d0028842b4c7567833
Yaseen <mhdyaz@outlook.com>
2021-04-05 18:18:21 +0300

    Edit and Like complete

---
+++ scripts/post.js
@@ -0,0 +1,39 @@
+
+var numLikes = 0;
+
+function editMode(){
+    document.getElementById("idPostTitle").setAttribute("contentEditable", "true")
+    document.getElementById("idPostTitle").setAttribute("style", "border : 2px solid red;")
+    document.getElementById("idPostText").setAttribute("contentEditable", "true")
+    document.getElementById("idPostText").setAttribute("style", "border : 2px solid red;")
+    document.getElementById("idEditButton").innerHTML= 'Save <i class="fa fa-save white-color " ></i>';
+    document.getElementById("idEditButton").setAttribute("onclick","save()");
+    
+
+}
+
+function save(){
+    document.getElementById("idPostTitle").setAttribute("contentEditable", "false")
+    document.getElementById("idPostTitle").removeAttribute("style")
+    document.getElementById("idPostText").setAttribute("contentEditable", "false")
+    document.getElementById("idPostText").removeAttribute("style");
+    document.getElementById("idEditButton").innerHTML= 'Edit <i class="fa fa-edit white-color " ></i>';
+    document.getElementById("idEditButton").setAttribute("onclick","editMode()");
+}
+
+function onLiked(){
+    ++numLikes;
+    document.getElementById("idLikeButton").innerHTML= '<i class="fa fa-thumbs-up white-color"></i> Liked'
+    document.getElementById("idLikeButton").innerHTML= '<i class="fa fa-thumbs-up white-color"></i> Liked'
+    
+    if (numLikes == 1 ){
+        document.getElementById("idLikeText").innerHTML= "1 person likes this!";
+    } else {
+        document.getElementById("idLikeText").innerHTML= numLikes + " people likes this!";
+    }
+
+}
+
+function addComment(comment) {
+    document.getElementById("idLikeButton").innerHTML += '<p class="comment">'+comment.value+ '</p>';
+}
